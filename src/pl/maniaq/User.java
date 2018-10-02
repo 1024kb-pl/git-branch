0514884fc4dd0d355c7e72da54655fa3b2e76e08
klimson <klimson@gitlab.com>
2018-10-02 19:28:39 +0200

    Create User class

---
+++ src/pl/maniaq/User.java
@@ -0,0 +1,22 @@
+package pl.maniaq;
+
+public class User {
+    private String name;
+    private String lastname;
+    private Integer age;
+
+    public User(String name, String lastname, Integer age) {
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
