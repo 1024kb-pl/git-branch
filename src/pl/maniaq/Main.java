ed4163a50658c78d7339a1b93fc05cc265ce2e93
klimson <klimson@gitlab.com>
2018-10-02 19:32:00 +0200

    Implement create user method in main class

--- src/pl/maniaq/Main.java
+++ src/pl/maniaq/Main.java
@@ -1,6 +1,26 @@
 package pl.maniaq;
+import java.util.Scanner;
 
 public class Main {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void createUser() {
+        String name, lastname;
+        Integer age;
+
+        System.out.println("Type a name: ");
+        name = scanner.next();
+
+        System.out.println("Type a lastname: ");
+        lastname = scanner.next();
+
+        System.out.println("Type your age: ");
+        age = scanner.nextInt();
+
+        User user = new User(name, lastname, age);
+        System.out.println("Utworzono Usera: " + user.toString());
+    }
 
     public static void main(String[] args) {
 	// write your code here
